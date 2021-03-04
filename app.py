2e18761677d2a79dbedea76e461e2964f8a65653
Sam Merham <samehisaac@yahoo.com>
2021-03-03 16:24:24 -0800

    second route

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template, request
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask_debugtoolbar import DebugToolbarExtension
 
 from stories import silly_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
-debug = DebugToolbarExtension(app)
+# debug = DebugToolbarExtension(app)
+
+
+
+@app.route('/')
+def prompt():
+    """ prompt user for words"""
+    prompts = silly_story.prompts
+    print(prompts)
+    return render_template("questions.html", prompts=prompts)
+
+@app.route('/story.html')
